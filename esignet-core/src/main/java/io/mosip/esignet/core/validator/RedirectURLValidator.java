a31a6e452fa5297bfd2b7e434c13f8068cd21707
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-09-05 23:39:17 +0530

    Update RedirectURLValidator.java
    Signed-off-by: Dharanidharan-S-04 <dean12345.dd@gmail.com>

--- esignet-core/src/main/java/io/mosip/esignet/core/validator/RedirectURLValidator.java
+++ esignet-core/src/main/java/io/mosip/esignet/core/validator/RedirectURLValidator.java
@@ -1,5 +1,6 @@
 package io.mosip.esignet.core.validator;
 
+import org.apache.commons.validator.routines.RegexValidator;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.stereotype.Component;
@@ -13,12 +14,16 @@ import static org.apache.commons.validator.routines.UrlValidator.ALLOW_LOCAL_URL
 @Component
 public class RedirectURLValidator implements ConstraintValidator<RedirectURL, String> {
 
-    private final UrlValidator urlValidator = new UrlValidator(ALLOW_ALL_SCHEMES+ALLOW_LOCAL_URLS);
+    // Matches host.tld structure without restricting to IANA-registered TLDs,
+    // so dev/test domains like *.test, *.tst, *.local etc. are accepted.
+    private static final RegexValidator AUTHORITY_VALIDATOR =
+            new RegexValidator("^([a-zA-Z0-9]([a-zA-Z0-9\\-]*[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,}$");
+
+    private final UrlValidator urlValidator =
+            new UrlValidator(null, AUTHORITY_VALIDATOR, ALLOW_ALL_SCHEMES + ALLOW_LOCAL_URLS);
 
     @Override
     public boolean isValid(String redirectUrl, ConstraintValidatorContext constraintValidatorContext) {
         return urlValidator.isValid(redirectUrl);
     }
-
-
 }
